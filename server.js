e0c5c9c22a6d73fd0aa3f145ab821e0b9f1edb7d
William Conway <wconway@umd.edu>
2024-11-05 17:00:18 -0500

    update port

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
 
 app.use(express.static("public"));
 
